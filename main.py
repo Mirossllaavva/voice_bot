acf455f7b3500b4083ffe7723797fad1adbe3036
Mirossllaavva <miroslavahavrilenko@gmail.com>
2025-11-29 00:26:34 +0200

    Add files via upload

--- main.py
+++ main.py
@@ -1,13 +1,13 @@
-import speech_recognition as sr
-recogniser = sr.Recognizer()
-def listening_voice():
-    with sr.Microphone() as sourse:
+import speech_recognition as sr #Підкключаєио модуль для розпізновання мови
+recogniser = sr.Recognizer() #створюємо розпізновач
+def listening_voice(): #функція для прослуховування аудіо
+    with sr.Microphone() as sourse: #підключення до мікрофону, як до істочнику
         print("Слухаю")
-        audio =  recogniser.listen(sourse)
-    return audio
-def voice_in_text(audio):
+        audio =  recogniser.listen(sourse) #зберігаємо аудіо 
+    return audio #повертаємо аудіо
+def voice_in_text(audio): #функція для перетворення аудіо на текст
     try:
-        text = recogniser.recognize_google(audio)
+        text = recogniser.recognize_google(audio) #Розпізнаємо текст та зберігаємо
         print(text)
     except:
         pass
