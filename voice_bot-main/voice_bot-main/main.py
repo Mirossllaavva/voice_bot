eb0129d7261d349dcc8a0ef92d2a27fa88872a7f
Mirossllaavva <miroslavahavrilenko@gmail.com>
2025-12-06 10:49:44 +0200

    Add files via upload
    добавила ИИ

---
+++ voice_bot-main/voice_bot-main/main.py
@@ -0,0 +1,48 @@
+#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 
+from groq import Groq
+import speech_recognition as sr #Підкключаєио модуль для розпізновання мови
+import subprocess
+import webbrowser 
+client = Groq(api_key = "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")
+recogniser = sr.Recognizer() #створюємо розпізновач
+def listening_voice(): #функція для прослуховування аудіо
+    with sr.Microphone() as sourse: #підключення до мікрофону, як до істочнику
+        print("Слухаю")
+        audio =  recogniser.listen(sourse) #зберігаємо аудіо 
+    return audio #повертаємо аудіо
+def voice_in_text(audio): #функція для перетворення аудіо на текст
+    try:
+        text = recogniser.recognize_google(audio, language = "uk-UK") #Розпізнаємо текст та зберігаємо
+        print(text)
+        return text
+    except:
+        pass
+def answer_bot(text):
+    if "привіт" in text.lower():
+    
+        response = client.chat.completions.create(model = "llama-3.3-70b-versatile",
+                                            messages = [{"role": "user", "content": text }])
+        print(response.choices[0].message.content)
+    # if "привіт" in text.lower():
+    #     print("Привіт. Чим я можу допомогти?")
+    # if "2 + 2" in text.lower():
+    #     print("4")
+    # if "німеччини" in text.lower():
+    #     print("Берлін")
+    if "калькулятор" in text.lower():
+        subprocess.call(["calc"])
+    if "гугл" in text.lower() or "google" in text.lower():
+        subprocess.call(["C:\Program Files\Google\Chrome\Application\chrome.exe"])
+    if "класрум" in text.lower() or "classroom" in text.lower():
+        webbrowser.open("https://classroom.google.com/u/0/h?hl=ruhttps://classroom.google.com/u/0/h?hl=ru")
+    # if "cутінки" in text.lower():
+    #     webbrowser.open(f'https://music.youtube.com/search?q={text.lower()[7:]}')
+    if "youtube" or "ютуб" in text.lower():
+        webbrowser.open("https://music.youtube.com/")
+    
+def main():
+    result_audio = listening_voice()
+    result_text = voice_in_text(result_audio)
+    answer_bot(result_text)
+if __name__ == "__main__":
+    main()
