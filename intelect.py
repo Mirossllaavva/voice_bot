4de7661d8afe912c6b27b089fb9e99e0c5b4dbdc
Mirossllaavva <miroslavahavrilenko@gmail.com>
2026-01-31 11:08:44 +0200

    Update API key and clean up code in intelect.py

--- intelect.py
+++ intelect.py
@@ -1,10 +1,12 @@
-#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 
+
+#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
 from groq import Groq
-client = Groq(api_key = "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")
+client = Groq(api_key = "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")
+
+
+
 def generator(promt):
     response = client.chat.completions.create(messages = [{"role": "user", "content": promt }],
                                              model = "llama-3.1-8b-instant")
                                          
     return response.choices[0].message.content
-
-
